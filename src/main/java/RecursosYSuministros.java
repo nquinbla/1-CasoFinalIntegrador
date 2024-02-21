f160a6c85b95cf12c946c38a76d7eb5ca2c4e815
nquinbla <nquinbla@myuax.com>
2024-02-21 11:22:03 +0100

    a

--- src/main/java/RecursosYSuministros.java
+++ src/main/java/RecursosYSuministros.java
@@ -1,2 +1,9 @@
 public class RecursosYSuministros {
+    private double resursoVital;
+    private double consumoDiario;
+
+    public RecursosYSuministros(double recursoVital, double consumoDiario) {
+        this.resursoVital = recursoVital;
+        this.consumoDiario = consumoDiario;
+    }
 }
