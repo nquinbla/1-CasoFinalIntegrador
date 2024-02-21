ec8059e78255efb2d64a021e04c3b4b12559c241
nquinbla <nquinbla@myuax.com>
2024-02-21 11:04:21 +0100

    a

--- src/main/java/CronómetroCósmico.java
+++ src/main/java/CronómetroCósmico.java
@@ -1,2 +1,11 @@
 public class CronómetroCósmico {
+    private double cicloDiarioPlaneta; // para ciclo diario (en días)
+    private double cicloAnualPlaneta; // para ciclo anual (en años)
+
+    public CronómetroCósmico(double cicloDiarioPlaneta, double cicloAnualPlaneta) {
+        this.cicloDiarioPlaneta = cicloDiarioPlaneta;
+        this.cicloAnualPlaneta = cicloAnualPlaneta;
+    }
+
+
 }
